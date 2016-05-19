6cb6492f2cf736f5c26aaf7b08b8a00cec3dd40d
francoandrew6 <francoandrew6@gmail.com>
2016-05-19 04:48:56 -0600

    Ejemplo
    Primer programa en android

---
+++ app/src/main/java/org/emmanuel/cafeteria/MainActivity.java
@@ -0,0 +1,30 @@
+package org.emmanuel.cafeteria;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText txtPedido=null;
+    private Button btnPedido=null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtPedido=(EditText) findViewById(R.id.txtPedir);
+        btnPedido=(Button)findViewById(R.id.btnPedido);
+        btnPedido.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Snackbar.make(v, "Click en pedido", Snackbar.LENGTH_SHORT).show();
+                Intent intent=new Intent(MainActivity.this,Factura.class);
+                intent.putExtra("pedido",txtPedido.getText().toString());
+                startActivity(intent);
+            }
+        });
+    }
+}
